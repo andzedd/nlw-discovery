c4b27915fea79717299cfd454ea8fff1a6fbf9bd
Matheus S Souza <andzedd.matheus@gmail.com>
2020-10-22 18:47:49 -0300

    add and remove images js

--- public/js/page-create-orphanage.js
+++ public/js/page-create-orphanage.js
@@ -21,9 +21,42 @@ map.on('click', (e) => {
     const lat = e.latlng.lat;
     const lng = e.latlng.lng;
 
+    document.querySelector('[name=lat]').value = lat;
+    document.querySelector('[name=lng]').value = lng;
+
     // clear map before adding icon
     marker && map.removeLayer(marker);
 
     // add icon marker
     marker = L.marker([lat,lng], { icon }).addTo(map);
 });
+
+// add and delete photos
+function addPhotos(){
+    const imagesContainer = document.querySelector('#images');
+
+    const uploads = document.querySelectorAll('.new-upload');
+
+    const cloneLastUpload = uploads[uploads.length - 1].cloneNode(true);
+
+    if(cloneLastUpload.children[0].value != ""){
+        cloneLastUpload.children[0].value = "";
+
+        imagesContainer.appendChild(cloneLastUpload);
+    }
+}
+
+function removePhoto(e){
+    const deleteButton = e.currentTarget;
+
+    const uploads = document.querySelectorAll('.new-upload').length;
+
+    if(uploads <= 1){
+        deleteButton.previousSibling.previousSibling.value = ""
+        return
+    }
+
+    deleteButton.parentNode.remove()
+
+
+}
