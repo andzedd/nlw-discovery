482afe3fe60604b7521373be5010dd6a9af9b99e
Matheus S Souza <andzedd.matheus@gmail.com>
2020-10-26 18:07:11 -0300

    saveOrphanage file

--- src/pages.js
+++ src/pages.js
@@ -1,16 +1,42 @@
-const orphanages = require("./database/fakedata.js");
+const Database = require('./database/db');
+const saveOrphanage = require('./database/saveOrphanage');
 
 module.exports = {
     index(req,res) {
         return res.render('index')
     },
-    orphanages(req,res) {
-        return res.render('orphanages', {orphanages})
+    
+    async orphanages(req,res) {
+        try {
+            const db = await Database;
+            const orphanages = await db.all("SELECT * FROM orphanages");
+            return res.render('orphanages', {orphanages});
+        } catch (error) {
+            console.log(error)
+            return res.send('Erro no banco de dados')
+        }
     },
+
     createOrphanage(req,res) {
         return res.render('create-orphanage')
     },
-    orphanage(req,res) {
-        return res.render('orphanage')
+
+    async orphanage(req,res) {
+        const id = req.query.id
+        try {
+            const db = await Database;
+            const result = await db.all(`SELECT * FROM orphanages WHERE id="${id}"`);
+            const orphanage = result[0]
+
+            orphanage.images = orphanage.images.split(',') 
+            orphanage.firstImage = orphanage.images[0]
+
+            orphanage.open_on_weekends == "0" ? orphanage.open_on_weekends = false : orphanage.open_on_weekends = true
+
+            return res.render('orphanage', {orphanage})
+        } catch (error) {
+            console.log(error)
+            return res.send("Erro no banco de dados")
+        }
     }
 }
